6dae4e2477d72b2265436caad9c6455edc988f5a
nguyenvuhoangbt <55265803+nguyenvuhoangbt@users.noreply.github.com>
2024-06-09 16:23:45 +0700

    huhu

--- src/components/banner/Horizontal.tsx
+++ src/components/banner/Horizontal.tsx
@@ -20,30 +20,30 @@ const Horizontal = () => {
   }, []);
 
   return (
-    <div w-pos="relative" w-overflow="hidden" w-h="150px md:300px">
-      {isClient ? (
-        <div w-flex="~" w-h="full" w-overflow="hidden">
-          {[...Array(2)].map((x, i) => (
-            <section
-              w-flex="~"
-              w-gap="20"
-              w-p="x-10"
-              w-animate="infinite-horizontal"
-              key={i}
-            >
-              <HTML5 w-h="100px md:200px" w-m="auto" />
-              <CSS3 w-h="100px md:200px" w-m="auto" />
-              <Typescript w-h="50px md:100px" w-m="auto" />
-              <PostCSS w-h="75px md:150px" w-m="auto" />
-              <Gitlab w-h="65px md:130px" w-m="auto" />
-              <React w-h="75px md:150px" w-m="auto" />
-              <Vue w-h="75px md:150px" w-m="auto" />
-              <PostgreSQL w-h="85px md:170px" w-m="auto" />
-              <GraphQL w-h="85px md:170px" w-m="auto" />
-            </section>
-          ))}
-        </div>
-      ) : null}
+    <div w-pos="relative" w-flex="~" w-overflow="hidden" w-h="150px md:300px">
+      {/* {isClient ? ( */}
+      {/* <div w-flex="~" w-h="full" w-overflow="hidden"> */}
+      {[...Array(2)].map((x, i) => (
+        <section
+          w-flex="~"
+          w-gap="20"
+          w-p="x-10"
+          w-animate="infinite-horizontal"
+          key={i}
+        >
+          <HTML5 w-h="100px md:200px" w-m="auto" />
+          <CSS3 w-h="100px md:200px" w-m="auto" />
+          <Typescript w-h="50px md:100px" w-m="auto" />
+          <PostCSS w-h="75px md:150px" w-m="auto" />
+          <Gitlab w-h="65px md:130px" w-m="auto" />
+          <React w-h="75px md:150px" w-m="auto" />
+          <Vue w-h="75px md:150px" w-m="auto" />
+          <PostgreSQL w-h="85px md:170px" w-m="auto" />
+          <GraphQL w-h="85px md:170px" w-m="auto" />
+        </section>
+      ))}
+      {/* </div> */}
+      {/* ) : null} */}
     </div>
   );
 };
